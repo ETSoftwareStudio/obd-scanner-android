a37d6b85b82779fa888ba702cf8bb6cce19bfe1f
Elton Viana <me@eltonvs.com>
2026-03-21 04:15:28 -0400

    Cache dashboard metric snapshots in repository state flow

--- app/src/main/kotlin/com/eltonvs/obdapp/domain/usecase/ReadMetricsUseCase.kt
+++ app/src/main/kotlin/com/eltonvs/obdapp/domain/usecase/ReadMetricsUseCase.kt
@@ -1,17 +1,17 @@
 package com.eltonvs.obdapp.domain.usecase
 
-import com.eltonvs.obdapp.domain.model.VehicleMetric
+import com.eltonvs.obdapp.domain.model.DashboardMetricsSnapshot
 import com.eltonvs.obdapp.domain.repository.ObdRepository
 import javax.inject.Inject
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 class ReadMetricsUseCase
     @Inject
     constructor(
         private val repository: ObdRepository,
     ) {
-        operator fun invoke(): Flow<VehicleMetric> {
-            return repository.vehicleMetrics
+        operator fun invoke(): StateFlow<DashboardMetricsSnapshot> {
+            return repository.dashboardMetrics
         }
 
         suspend fun startPolling(intervalMs: Long) {
