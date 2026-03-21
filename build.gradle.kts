92e33ea384e27036e729b448cd615f54dd395871
Elton Viana <me@eltonvs.com>
2026-03-20 22:17:28 -0400

    Bump KSP plugin patch for built-in Kotlin compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.dagger.hilt.android") version "2.59" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
