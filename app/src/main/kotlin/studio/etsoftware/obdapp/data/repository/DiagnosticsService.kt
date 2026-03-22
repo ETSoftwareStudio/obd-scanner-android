d41177395c858b2741b1a5599c4c1d953fa5857e
Elton Viana <me@eltonvs.com>
2026-03-22 04:26:50 -0400

    refactor: extract diagnostics service

---
+++ app/src/main/kotlin/studio/etsoftware/obdapp/data/repository/DiagnosticsService.kt
@@ -0,0 +1,68 @@
+package studio.etsoftware.obdapp.data.repository
+
+import com.github.eltonvs.obd.command.control.ResetTroubleCodesCommand
+import com.github.eltonvs.obd.command.control.TroubleCodesCommand
+import com.github.eltonvs.obd.command.control.VINCommand
+import com.github.eltonvs.obd.connection.ObdDeviceConnection
+import javax.inject.Inject
+import javax.inject.Singleton
+import studio.etsoftware.obdapp.domain.model.DiagnosticInfo
+import studio.etsoftware.obdapp.domain.model.TelemetryContext
+import studio.etsoftware.obdapp.domain.model.TroubleCode
+import studio.etsoftware.obdapp.domain.model.TroubleCodeType
+import studio.etsoftware.obdapp.util.LogManager
+
+@Singleton
+class DiagnosticsService
+    @Inject
+    constructor(
+        private val commandExecutor: ObdCommandExecutor,
+        private val dtcParser: DtcParser,
+        private val logManager: LogManager,
+    ) {
+        suspend fun readDiagnosticInfo(connection: ObdDeviceConnection): DiagnosticInfo {
+            val vin =
+                commandExecutor.execute(
+                    context = TelemetryContext.DIAGNOSTICS,
+                    cycleId = null,
+                    rawPid = "VIN",
+                    commandName = "VINCommand",
+                    block = { connection.run(VINCommand()) },
+                    preview = { response -> response.value },
+                )
+
+            val troubleCodes =
+                commandExecutor.execute(
+                    context = TelemetryContext.DIAGNOSTICS,
+                    cycleId = null,
+                    rawPid = "03",
+                    commandName = "TroubleCodesCommand",
+                    block = { connection.run(TroubleCodesCommand()) },
+                    preview = { response -> response.value },
+                )
+
+            val codes = dtcParser.parse(troubleCodes.value)
+
+            return DiagnosticInfo(
+                vin = vin.value,
+                troubleCodes =
+                    codes.map { code ->
+                        TroubleCode(code, dtcParser.descriptionFor(code), TroubleCodeType.CURRENT)
+                    },
+                milStatus = codes.isNotEmpty(),
+                dtcCount = codes.size,
+            )
+        }
+
+        suspend fun clearTroubleCodes(connection: ObdDeviceConnection) {
+            logManager.command("04 (Clear trouble codes)")
+            commandExecutor.execute(
+                context = TelemetryContext.CLEAR_DTC,
+                cycleId = null,
+                rawPid = "04",
+                commandName = "ResetTroubleCodesCommand",
+                block = { connection.run(ResetTroubleCodesCommand()) },
+            )
+            logManager.success("Trouble codes clear command sent")
+        }
+    }
