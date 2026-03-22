91510913467977a43dcd3238f5c0540ad305a747
Elton Viana <me@eltonvs.com>
2026-03-22 15:01:45 -0400

    refactor: keep dashboard placeholder formatting in presentation

--- app/src/main/kotlin/studio/etsoftware/obdapp/domain/model/DashboardMetricsSnapshot.kt
+++ app/src/main/kotlin/studio/etsoftware/obdapp/domain/model/DashboardMetricsSnapshot.kt
@@ -1,11 +1,11 @@
 package studio.etsoftware.obdapp.domain.model
 
 data class DashboardMetricsSnapshot(
-    val speed: String = "--",
-    val rpm: String = "--",
-    val throttle: String = "--",
-    val coolantTemp: String = "--",
-    val intakeTemp: String = "--",
-    val maf: String = "--",
-    val fuel: String = "--",
+    val speed: String = "",
+    val rpm: String = "",
+    val throttle: String = "",
+    val coolantTemp: String = "",
+    val intakeTemp: String = "",
+    val maf: String = "",
+    val fuel: String = "",
 )
